de91c19879aa87c61100ff51aa4ce91170a45d4b
Jacob Hebert <39713480+jakeHebert@users.noreply.github.com>
2019-02-23 01:56:06 -0500

    update

--- DarkLyricsScraper.py
+++ DarkLyricsScraper.py
@@ -5,7 +5,8 @@ from darklyrics import *
 window = Tk()
 window.title("DarkLyric Scraper")
 
-artistPrompt = Label(window, text="What is the Artist you are looking for?")
+
+artistPrompt = Label(window, text="Artist")
 artistPrompt.grid(column=0, row=1)
 
 artistName = StringVar()
@@ -18,10 +19,7 @@ songsCB = ttk.Combobox(window, width=27, state="readonly", textvariable=songName
 
 lyrics = StringVar()
 lyricsLabel = Label(window, text="", textvariable=lyrics)
-lyricsLabel.grid(column=0, row=4)
 
-blah = get_songs("kayo dot")
-print(blah)
 
 def getAlbums(self):
     try:
@@ -46,6 +44,7 @@ def getSongs(self):
 
 
 def displayLyrics(self):
+    lyricsLabel.grid(column=0, row=4, columnspan=4)
     lyrics.set(get_lyrics(songName.get(), artistName.get()))
 
 
@@ -58,9 +57,9 @@ def unlockArtist():
     songsCB.configure(state=DISABLED)
 
 
-songPrompt = Label(window, text="What is the Song you are looking for?")
+songPrompt = Label(window, text="Song")
 unlockBtn = Button(window, text="New Artist", command=unlockArtist)
-albumPrompt = Label(window, text="What is the Album you are looking for?")
+albumPrompt = Label(window, text="Album")
 
 artistEntry.bind("<Return>", getAlbums)
 albumsCB.bind("<<ComboboxSelected>>", getSongs)
